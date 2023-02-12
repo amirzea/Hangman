d5786694f692cd0aa2182b2f29ee44c8445194da
Adelina Mirzea <88186597+amirzea@users.noreply.github.com>
2023-02-12 18:00:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let word
-let letter;
-let attempts = 6, letters_guessed = 0;
+let word, attempts = 6, letters_guessed = 0;
+let board = document.getElementById("board");
 
 function savedWord() {
     word = document.getElementById("userInput").value;
-    document.getElementById("board").innerHTML = "";
+    board.innerHTML = "";
     generateBoard();
 }
 
@@ -12,50 +11,49 @@ function generateBoard() {
     document.getElementById("pictureHangman").src = attempts + ".jpg";
     document.getElementById("title").innerHTML = "Hangman";
     for (let i = 0; i < word.length; ++i) {
-        document.getElementById("board").innerHTML +=
-        `
-        <u style="display: inline-block" class="${word.charAt(i)}">_</u>
-        `;
+       board.innerHTML +=
+       `<u style="display: inline-block" class="${word.charAt(i)}">_</u>
+       `;
     }    
-    document.getElementById("board").innerHTML += `<br><br>`;    
+    board.innerHTML += `<br><br>`;    
     for (let i = 97; i <= 122; ++i) {
-        letter = String.fromCharCode(i);
-		document.getElementById("board").innerHTML += 
-        `
-        <button style="border-radius:10px;" class="btn btn-light" id="${letter}" onclick="refreshBoard(this.id)">${letter}</button>
+	board.innerHTML += 
+        `<button style="border-radius:10px;" class="btn btn-light" id="${String.fromCharCode(i)}" onclick="refreshBoard(this.id)">${String.fromCharCode(i)}</button>
         `;
     } 
 }
 
 function refreshBoard(elementID) {
     document.getElementById(elementID).disabled = true;
-    let letters_array = document.getElementsByClassName(elementID);
-    for (let i = 0; i < letters_array.length; ++i) {
-        let item = letters_array[i];  
-        item.innerHTML = elementID;
-    }
     if (word.includes(elementID) == false) {
         --attempts;
-        console.log(attempts);
         document.getElementById("pictureHangman").src = attempts + ".jpg";
-        if(attempts == 0) {
+        if (attempts == 0) {
             youLost();
         }
-    }
-    letters_guessed += letters_array.length;
-    if (letters_guessed == word.length) {
-        youWon();
+    } else {
+        for (let i = 0; i < letters_array.length; ++i) {
+            document.getElementsByClassName(elementID)[i].innerHTML = elementID;
+        }
+        letters_guessed += document.getElementsByClassName(elementID).length;
+        if (letters_guessed == word.length) {
+            youWon();
+        }
     }
 }
 
 function youLost() {
-    document.getElementById("endGame").innerHTML = "Sorry, you lost &#128533" + 
-    `<br><button class="btn btn-info" onclick="history.go(0)"> Play again </br>
-    `;
+    document.getElementById("endGame").innerHTML = "Sorry, you lost &#128533";
+    reload();
 }
 
 function youWon() {
-    document.getElementById("endGame").innerHTML = "Congrats, you won &#127881" + 
+    document.getElementById("endGame").innerHTML = "Congrats, you won &#127881";
+    reload();
+}
+
+function reload() {
+    document.getElementById("endGame").innerHTML +=
     `<br><button class="btn btn-info" onclick="history.go(0)"> Play again </br>
     `;
 }
